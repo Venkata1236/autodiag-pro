cee8de946f7003d3184687384ab773fcce3b1094
Venkata Reddy <bommavaramvenkat2003@gmail.com>
2026-05-02 19:00:43 +0530

    feat: add PostgreSQL table initialization script

---
+++ backend/app/database/init_db.py
@@ -0,0 +1,19 @@
+import asyncio
+
+from app.database.connection import (
+    Base,
+    engine
+)
+
+from app.database import models
+
+
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(
+            Base.metadata.create_all
+        )
+
+
+if __name__ == "__main__":
+    asyncio.run(init_db())
