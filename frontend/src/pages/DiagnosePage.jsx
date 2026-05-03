6dcdc42cfbd280abece08f8c9566f568de949649
Venkata Reddy <bommavaramvenkat2003@gmail.com>
2026-05-03 06:28:42 +0530

    feat: create vehicle diagnosis page

---
+++ frontend/src/pages/DiagnosePage.jsx
@@ -0,0 +1,21 @@
+import VehicleForm from "../components/VehicleForm"
+
+
+function DiagnosePage() {
+
+  return (
+    <div className="
+      min-h-screen
+      bg-slate-950
+      p-10
+      text-white
+    ">
+
+      <VehicleForm />
+
+    </div>
+  )
+}
+
+
+export default DiagnosePage
