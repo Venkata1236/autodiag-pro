eb3c7ce173dee9a9bc0088ffcc5dfda66dd5ea50
Venkata Reddy <bommavaramvenkat2003@gmail.com>
2026-05-03 06:15:24 +0530

    feat: create frontend API service layer

---
+++ frontend/src/services/api.js
@@ -0,0 +1,19 @@
+import axios from "axios"
+
+const api = axios.create({
+  baseURL: "http://127.0.0.1:8000/api"
+})
+
+export const diagnoseVehicle = async (
+  payload
+) => {
+
+  const response = await api.post(
+    "/diagnose",
+    payload
+  )
+
+  return response.data
+}
+
+export default api
